fee37197aca76fdd588bb6bb1b6e27f10fc034d5
Eric Murphy <hi@ericnmurphy.com>
2019-03-08 20:07:13 +0700

    Added profile image and styled bio

--- src/components/bio.js
+++ src/components/bio.js
@@ -1,38 +1,81 @@
 import React from "react"
+import { StaticQuery } from "gatsby"
+import Img from "gatsby-image"
+import styled from "styled-components"
 
 import Subscribe from "./subscribe"
 
+const BioWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 150px 1fr;
+  column-gap: 2rem;
+  margin-bottom: 2rem;
+
+  @media (max-width: 700px) {
+    grid-template-columns: 1fr;
+  }
+
+  .image-wrapper {
+    max-width: 150px;
+    margin-bottom: 1.5rem;
+  }
+
+  img {
+    border-radius: 50%;
+  }
+`
+
 const Bio = () => (
-  <>
-    <p>
-      Want to become a better developer?{" "}
-      <span role="img" aria-label="Laptop">
-        💻
-      </span>{" "}
-      I'm Eric and I write about web development &amp; making web apps to help
-      you level up your skills and learn to build cool things online.
-    </p>
-    <p>
-      Subscribe{" "}
-      <span role="img" aria-label="Letter">
-        💌
-      </span>{" "}
-      below and I'll send you an email every Friday about how to become a better
-      developer and create some cool projects.
-    </p>
-    <Subscribe />
-    <p>
-      I also post a lot on Twitter, follow me at{" "}
-      <a
-        target="_blank"
-        rel="noopener noreferrer"
-        href="https://twitter.com/ericnmurphy"
-      >
-        @ericnmurphy
-      </a>
-      .
-    </p>
-  </>
+  <BioWrapper>
+    <div className="image-wrapper">
+      <StaticQuery
+        query={graphql`
+          query {
+            placeholderImage: file(relativePath: { eq: "profile.jpg" }) {
+              childImageSharp {
+                fluid(maxWidth: 150) {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+          }
+        `}
+        render={data => (
+          <Img fluid={data.placeholderImage.childImageSharp.fluid} />
+        )}
+      />
+    </div>
+    <div>
+      <h1>Want to become a better developer?</h1>{" "}
+      <p>
+        I'm Eric and I write about front-end development to help you level up
+        your skills and learn to build cool things online.{" "}
+        <span role="img" aria-label="Laptop">
+          💻
+        </span>
+      </p>
+      <p>
+        Subscribe{" "}
+        <span role="img" aria-label="Letter">
+          💌
+        </span>{" "}
+        below and I'll send you an email every Friday about how to become a
+        better developer and build your skills!
+      </p>
+      <Subscribe />
+      <p>
+        I also post a lot on Twitter, follow me at{" "}
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://twitter.com/ericnmurphy"
+        >
+          @ericnmurphy
+        </a>
+        .
+      </p>
+    </div>
+  </BioWrapper>
 )
 
 export default Bio

ef26cc4c1095b5b001d7d042d887321143606cd5
Eric Murphy <eric@ericmurphy.xyz>
2019-09-21 11:26:56 +0700

    Set up basic homepage layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,41 @@
 import React from "react"
+import styled from "styled-components"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
+const Hero = styled.header`
+  display: grid;
+  max-width: 1120px;
+  height: 100vh;
+  margin: 0 auto;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 4em;
+  align-items: center;
+`
+
+const Column = styled.div``
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
+    <Hero>
+      <Column>
+        <Image />
+      </Column>
+      <Column>
+        <h1>Hey, I'm Eric Murphy.</h1>
+        <p>
+          I build blazing-fast, modern websites and webapps that are a joy to
+          use.
+        </p>
+        <p>
+          I also make videos teaching people how to become better, more
+          effective developers.
+        </p>
+      </Column>
+    </Hero>
   </Layout>
 )
 

01fb05bc452bef778c2a459aa81e8ac31a919333
Eric Murphy <ericnmurphy@gmail.com>
2019-11-10 14:57:51 +0700

    Added Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,6 +40,12 @@ module.exports = {
       },
     },
     `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: process.env.GANALYTICS_ID,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
